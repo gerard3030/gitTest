c2da603ea8f970f5dc64b6ca002da3afa3842590
Gerard Trigalo <gerardtrigalo@gmail.com>
2023-10-27 16:41:11 +0300

    third coomit

---
+++ newFile.js
@@ -0,0 +1,6 @@
+console.log("new file here")
+console.log("new file here2")
+console.log("new file here3")
+
+console.log("new print added")
+console.log("new print added2")
--- newFile2.js
+++ newFile2.js
@@ -1,2 +1,2 @@
-console.log("new file here G")
-console.log("new file here G!")
+console.log("new file here G33")
+console.log("new file here G33!")
